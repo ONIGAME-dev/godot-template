b2d843840e09dedebecd39973d2f3e3e7d165b90
Victor Savcenco <victor@onigame.dev>
2024-08-19 11:23:45 -0400

    updated addons list (added gut)

--- addons/addons.mk
+++ addons/addons.mk
@@ -1,19 +1,22 @@
 # addons.mk
 
 .PHONY: addons
-addons: addons/input_helper addons/dialogue_manager addons/quest_system addons/limboai addons/gloot
+addons: addons/gloot addons/dialogue_manager addons/gut addons/input_helper addons/limboai addons/quest_system
 
-addons/input_helper:
-	@$(call install_addon,2107)
+addons/gloot:
+	@$(call install_addon,1368)
 
 addons/dialogue_manager:
 	@$(call install_addon,1432)
 
-addons/quest_system:
-	@$(call install_addon,2516)
+addons/gut:
+	@$(call install_addon,1709)
+
+addons/input_helper:
+	@$(call install_addon,2107)
 
 addons/limboai:
 	@$(call install_addon,2514)
 
-addons/gloot:
-	@$(call install_addon,1368)
+addons/quest_system:
+	@$(call install_addon,2516)
