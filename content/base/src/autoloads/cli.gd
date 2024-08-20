76c36a6339f0a60cbd8dc997b72eb11accf4f98a
victor <victor@onigame.dev>
2024-08-20 02:05:43 +0000

    fixed var shadowing warnings

--- content/base/src/autoloads/cli.gd
+++ content/base/src/autoloads/cli.gd
@@ -1,16 +1,16 @@
 extends Node
 
-var args: Array[Arg] = [
+var arguments: Array[Arg] = [
   Arg.new({triggers=["-h", "--help", "-?"], help="show this help message and exit", action="store_true"})
 ]
 
 func _ready() -> void:
-  var p := Parser.new()
-  for i in args:
-    p.add_argument(i)
-  var args = p.parse_arguments(OS.get_cmdline_args() + OS.get_cmdline_user_args())
-  if args == null:
+  var _parser := Parser.new()
+  for i in arguments:
+    _parser.add_argument(i)
+  var _args = _parser.parse_arguments(OS.get_cmdline_args() + OS.get_cmdline_user_args())
+  if _args == null:
     get_tree().quit()
-  elif args.get("help", false):
-    print(p.help())
+  elif _args.get("help", false):
+    print(_parser.help())
     get_tree().quit()
